ce1c33bd7da7b0c2b2a0469535dd18be85743e19
Justin Hurst <jhurst@gmail.com>
2019-12-10 21:27:37 -0600

    Update Files

--- web/search.php
+++ web/search.php
@@ -59,13 +59,13 @@
                 $reportingBoundary = "No";
             }
             
-            echo '<p>';
-            echo '<b>Event ID:</b>  ';
-            echo '<a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a><br>';
-            echo '<b>Date Occurred:</b>  ' . $dateOccurred->format('M d, Y').'<br>';
-            echo '<b>Short Description:</b>  ' . $row['description_short'].'<br>';
-            echo '<b>Within Reporting Boundaries?:</b>  ' . $reportingBoundary;'<br>';
-            echo '</p>';
+            // echo '<p>';
+            // echo '<b>Event ID:</b>  ';
+            // echo '<a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a><br>';
+            // echo '<b>Date Occurred:</b>  ' . $dateOccurred->format('M d, Y').'<br>';
+            // echo '<b>Short Description:</b>  ' . $row['description_short'].'<br>';
+            // echo '<b>Within Reporting Boundaries?:</b>  ' . $reportingBoundary;'<br>';
+            // echo '</p>';
 
             
             echo '<tr><td><a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a></td><td>' . $dateOccurred->format('M d, Y').'</td><td>' . $row['description_short'].'</td><td>' . $reportingBoundary;'</td></tr>';
@@ -80,7 +80,8 @@
         $stmt->bindValue(':endDate', $_GET['endDate'], PDO::PARAM_INT);
         $stmt->execute();
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+        echo '<table>';
+        echo '<th>Event ID</th><th>Date Occurred</th><th>Short Description</th><th>Within Reporting Boundaries?</th>';
         foreach ($rows as $row)
         {
             $dateOccurred = new DateTime($row['date_occurred']);
@@ -94,14 +95,18 @@
                 $reportingBoundary = "No";
             }
             
-            echo '<p>';
-            echo '<b>Event ID:</b>  ';
-            echo '<a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a><br>';
-            echo '<b>Date Occurred:</b>  ' . $dateOccurred->format('M d, Y').'<br>';
-            echo '<b>Short Description:</b>  ' . $row['description_short'].'<br>';
-            echo '<b>Within Reporting Boundaries?:</b>  ' . $reportingBoundary;'<br>';
-            echo '</p>';
+            // echo '<p>';
+            // echo '<b>Event ID:</b>  ';
+            // echo '<a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a><br>';
+            // echo '<b>Date Occurred:</b>  ' . $dateOccurred->format('M d, Y').'<br>';
+            // echo '<b>Short Description:</b>  ' . $row['description_short'].'<br>';
+            // echo '<b>Within Reporting Boundaries?:</b>  ' . $reportingBoundary;'<br>';
+            // echo '</p>';
+
+            echo '<tr><td><a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a></td><td>' . $dateOccurred->format('M d, Y').'</td><td>' . $row['description_short'].'</td><td>' . $reportingBoundary;'</td></tr>';
+            
         }
+        echo '</table>';
     }
     ?>
     <br>
