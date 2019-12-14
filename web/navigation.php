ac7ecf3b974bad2f5cece820d84055b33e81c85f
Justin Hurst <jhurst@gmail.com>
2019-12-14 00:27:39 -0600

    Update Files

--- web/navigation.php
+++ web/navigation.php
@@ -2,5 +2,4 @@
     <a class="active" href="index.php">Home</a>
     <a class="active" href="new.php">Create New Event</a>
     <a class="active" href="search.php">Search</a>
-    <a class="active" href="report.php">Reports</a>
 </div>
