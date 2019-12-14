4d04e66aca1094fc1ffb8360d42a9224cb781b8a
Justin Hurst <jhurst@gmail.com>
2019-12-14 14:40:20 -0600

    Update Files

--- web/search.php
+++ web/search.php
@@ -33,12 +33,6 @@
 
         <input type="submit" value="Search Date Range" class="button">
     </form>
-    <br>OR<br><br>
-    <form>
-        Search by Short Description (contains):
-        <input type="text" name="short_description" value=description>
-        <input type="submit" value="Search Short Description" class="button">
-    </form>
 
     
     <?php
@@ -93,30 +87,7 @@
         }
         echo '</table>';
     }
-    if (isset($_GET['short_description']))
-    {
-        $stmt = $db->prepare('select * from events WHERE description_short LIKE %:short_description%');
-        $stmt->bindValue(':short_description', $_GET['short_description'], PDO::PARAM_INT);
-        $stmt->execute();
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo '<table class="table1">';
-        echo '<th>Event ID</th><th>Date Occurred</th><th>Short Description</th><th>Within Reporting Boundaries?</th>';
-        foreach ($rows as $row)
-        {
-            $dateOccurred = new DateTime($row['date_occurred']);
-            $reportingBoundary = $row['reporting_boundary'];
-            if ($reportingBoundary == 1)
-            {
-                $reportingBoundary = "Yes";
-            }
-            else
-            {
-                $reportingBoundary = "No";
-            }
-            echo '<tr><td><a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a></td><td>' . $dateOccurred->format('M d, Y').'</td><td>' . $row['description_short'].'</td><td>' . $reportingBoundary;'</td></tr>';
-        }
-        echo '</table>';
-    }
+
     ?>
     
     <br>
