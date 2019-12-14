9b2f4c1fe585c25ea0b948e809a0551517c273e2
Justin Hurst <jhurst@gmail.com>
2019-12-14 14:35:13 -0600

    Update Files

--- web/report.php
+++ web/report.php
@@ -17,27 +17,22 @@
     require "navigation.php";
     ?>
     <h2>Search</h2>
-    
     <form>
         Search by Event ID: 
+        <br>
         <input type="number" name="eventID" value=1001>
+        <br>        
         <input type="submit" value="Search Event ID" class="button">
     </form>
     <br>OR<br><br>
     <form>
         Search by Date Range: 
-
+        <br>
         Start Date:<input type="date" name="startDate" value="2019-01-01" required>
-
+        <br>
         End Date:<input type="date" name="endDate" value='2019-12-01' required>
-
+        <br>
         <input type="submit" value="Search Date Range" class="button">
-    </form>
-    <br>OR<br><br>
-    <form>
-        Search by Short Description (contains):
-        <input type="text" name="short_description" value=description>
-        <input type="submit" value="Search Short Description" class="button">
     </form>
 
     
@@ -93,32 +88,7 @@
         }
         echo '</table>';
     }
-    if (isset($_GET['short_description']))
-    {
-        $stmt = $db->prepare('select * from events WHERE CONTAINS(description_short, :short_description');
-        $stmt->bindValue(':short_description', $_GET['short_description'], PDO::PARAM_INT);
-        $stmt->execute();
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo '<table class="table1">';
-        echo '<th>Event ID</th><th>Date Occurred</th><th>Short Description</th><th>Within Reporting Boundaries?</th>';
-        foreach ($rows as $row)
-        {
-            $dateOccurred = new DateTime($row['date_occurred']);
-            $reportingBoundary = $row['reporting_boundary'];
-            if ($reportingBoundary == 1)
-            {
-                $reportingBoundary = "Yes";
-            }
-            else
-            {
-                $reportingBoundary = "No";
-            }
-            echo '<tr><td><a href="event-details.php?event_id=' . $row['event_id'] . '">'. $row['event_id'].'</a></td><td>' . $dateOccurred->format('M d, Y').'</td><td>' . $row['description_short'].'</td><td>' . $reportingBoundary;'</td></tr>';
-        }
-        echo '</table>';
-    }
     ?>
-    
     <br>
     </body>
 </html>
